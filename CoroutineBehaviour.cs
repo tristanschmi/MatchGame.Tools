1c1223016d7c495ed84a4c7aac6d00b010a80226
tristan_s <waitwhat314@gmail.com>
2024-09-17 01:39:10 -0600

    CoroutineBehaviour

---
+++ CoroutineBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public IntData counterNum;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+
+        startEvent.Invoke();
+        while (counterNum.value > 0)
+        {
+            Debug.Log(counterNum);
+            repeatEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+            
+        }
+        endEvent.Invoke();
+    }
+}
